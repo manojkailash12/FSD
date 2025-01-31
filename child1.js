bbc783b264300d1dde778c42f3d7b74e15bf47a7
Manoj <manoj@example.com>
2025-01-31 11:42:03 +0530

    pushing the codes to repo

---
+++ child1.js
@@ -0,0 +1,30 @@
+// import x from "./child1.js"
+//     let x = 10;
+
+    // export default x;
+
+    // let userDetail={
+    //     name:"Chaithu",
+    //     Profession:"Student",
+    // }
+    // export default userDetails;
+
+    //named export
+
+    let x = 10;
+    const user="tanuja";
+    const users=["sathwik" , "Lokesh" , "Harsha"];
+
+    const obj={
+        name:"Sam",
+        company:"ExcelR"
+    }
+    //named export
+    let college ="MallaReddyUniversity";
+    export default college;
+    export{
+        x,
+        user,
+        users,
+        obj
+    }
---
+++ script1.js
@@ -0,0 +1,4 @@
+export let msg ="Hello, Good Morning!!";
+export function greet(){
+    return msg+"Iam Coding Now"
+}
